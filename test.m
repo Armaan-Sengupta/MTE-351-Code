d835034675c3bb211bee68259ef2a65f61f5d35c
Armaan Sengupta <senguptaarmaan@gmail.com>
2025-03-17 19:20:54 -0400

    Working version

--- test.m
+++ test.m
@@ -2,7 +2,7 @@ clear all;
 clc;
 close all;
 
-syms x_ddot theta_ddot u M Mu I_Gw I_Gu R L_g g beta theta
+syms x_dot x_ddot theta_dot theta_ddot u M Mu I_Gw I_Gu R L_g g beta theta
 
 USE_EXPR_FOR_CONSTANTS = false; %change to false to get in terms of C1...C7 or to true to get expanded version
 
@@ -19,7 +19,7 @@ else
     syms C1 C2 C3 C4 C5 C6 C7;
 end
 % Given system of equations
-eq1 = C1*x_ddot + C2*theta_ddot - C3*theta_ddot^2 == C7*u;
+eq1 = C1*x_ddot + C2*theta_ddot - C3*theta_dot^2 == C7*u;
 eq2 = C4*theta_ddot + C5*x_ddot - C6 == 0;
 
 % Solve eq1 for x_ddot and eq2 for theta_ddot
@@ -27,14 +27,19 @@ x_ddot_isolated = solve(eq1, x_ddot);
 theta_ddot_isolated = solve(eq2, theta_ddot);
 
 % Substitute theta_ddot_isolated into x_ddot_isolated
-x_ddot_substituted = simplify(subs(x_ddot_isolated, theta_ddot, theta_ddot_isolated));
+x_ddot_substituted = x_ddot == simplify(subs(x_ddot_isolated, theta_ddot, theta_ddot_isolated));
 
 % Substitute x_ddot_isolated into theta_ddot_isolated
-theta_ddot_substituted = simplify(subs(theta_ddot_isolated, x_ddot, x_ddot_isolated));
+theta_ddot_substituted = theta_ddot == simplify(subs(theta_ddot_isolated, x_ddot, x_ddot_isolated));
+
+
+
+x_ddot_sub_isolated = solve(x_ddot_substituted, x_ddot);
+theta_ddot_sub_isolated = solve(theta_ddot_substituted, theta_ddot);
 
 % Construct LaTeX equations explicitly including the left-hand side
-eq1_latex = ['$ \ddot{x} = ', latex(x_ddot_substituted), ' $'];
-eq2_latex = ['$ \ddot{\theta} = ', latex(theta_ddot_substituted), ' $'];
+eq1_latex = ['$ \ddot{x} = ', latex(x_ddot_sub_isolated), ' $'];
+eq2_latex = ['$ \ddot{\theta} = ', latex(theta_ddot_sub_isolated), ' $'];
 
 % Display equations in figure with LaTeX rendering
 figure;
