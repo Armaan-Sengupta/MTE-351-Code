2a606996764f4297448e1082cb32b09c2b86476f
Armaan Sengupta <senguptaarmaan@gmail.com>
2025-03-24 17:43:28 -0400

    Version added without circular dependancy warning

--- init.m
+++ init.m
@@ -16,3 +16,15 @@ C1 = M + Mu + (I_Gw / R^2);
 C2 = L_g * Mu;                     % cos(beta) ≈ 1    % sin(beta) ≈ (theta + beta)
 C4 = (Mu * L_g^2 + I_Gu);
 C7 = 1; % Given
+
+% Defining constant A:
+A = 1 / (C1 + (C2 * g / C4));
+
+% Defining constant B:
+B = (C2 * g) / (C4 * C1 + C2 * g);
+
+% Defining constant C:
+C = C2 / (C4 * C1 + C2^2);
+
+% Defining constant D:
+D = (C2 * g) / (C4 + (C2^2 / C1));
