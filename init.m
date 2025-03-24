644119810f653f06fdf4186ff2b687be811c0ebc
Armaan Sengupta <senguptaarmaan@gmail.com>
2025-03-24 16:51:08 -0400

    First try at simulink model

---
+++ init.m
@@ -0,0 +1,17 @@
+clear all;
+clc;
+close all;
+
+M = 35;
+Mu = 50 + 1/2;
+I_Gw = 0.02;
+R = 12.7;
+L_g = 1;
+I_Gu = 20;
+g = 9.81;
+
+
+C1 = M + Mu + (I_Gw / R^2);
+C2 = L_g * Mu;                     % cos(beta) ≈ 1    % sin(beta) ≈ (theta + beta)
+C4 = (Mu * L_g^2 + I_Gu);
+C7 = 1; % Given
